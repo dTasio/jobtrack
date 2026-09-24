5c62fc8ef2f3f40b8aeca1023329e0b66079c8c9
diego <djtasio2002@gmail.com>
2026-09-24 09:26:24 +0200

    render opportunities and add theme toggle

--- js/app.js
+++ js/app.js
@@ -6,12 +6,27 @@ const estadoSelect = document.querySelector('#estado');
 const enlaceInput = document.querySelector('#enlace');
 const nextInput = document.querySelector('#next');
 
+const themeToggle = document.querySelector('#theme-toggle');
+const themeIcon = document.querySelector('#theme-icon');
+
+const listaOportunidades = document.querySelector('#lista-oportunidades');
+
 let oportunidades = [];
+themeToggle.addEventListener('click', cambiarTema);
 
 document.addEventListener('DOMContentLoaded', () => {
-          oportunidades = JSON.parse( localStorage.getItem('oportunidades') ) || []  ;
-          console.log(oportunidades);
-     });
+    oportunidades = JSON.parse( localStorage.getItem('oportunidades') ) || [];
+
+    renderizarOportunidades();
+
+    const temaGuardado = localStorage.getItem('theme');
+
+    if (temaGuardado) {
+        aplicarTema(temaGuardado);
+    } else {
+        aplicarTema('light');
+    }
+});
 
 formulario.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -42,8 +57,10 @@ formulario.addEventListener('submit', (event) => {
         };
 
         oportunidades.push(oportunidad);
+
         localStorage.setItem('oportunidades', JSON.stringify(oportunidades));
-        console.log('Oportunidades:', oportunidades);
+
+        renderizarOportunidades();
 
         limpiarFormulario();
 
@@ -65,6 +82,43 @@ function validarCampo(input) {
     return true;
 }
 
+//Renderizar oportunidades
+function renderizarOportunidades() {
+    if (oportunidades.length === 0) {
+        listaOportunidades.innerHTML = `
+            <div class="estado-vacio">
+                <p>Aún no hay oportunidades guardadas.</p>
+                <span>Añade tu primera oportunidad desde el formulario.</span>
+            </div>
+        `;
+
+        return;
+    }
+    const cardsHTML = oportunidades.map((oportunidad) => {
+    return `
+        <article class="oportunidad-card">
+            <div class="oportunidad-info">
+                <h3>${oportunidad.puesto}</h3>
+                <p class="empresa">${oportunidad.empresa}</p>
+                <p>Candidatura: <time class="time-info" datetime="${oportunidad.fecha}">${formatearFecha(oportunidad.fecha)}</time></p>
+                <a href="${oportunidad.enlace}" target="_blank rel="noopener noreferrer">Ver Oferta</a>
+            </div>
+            <div class="oportunidad-seguimiento">
+                <span class="estado estado-${oportunidad.estado}">${oportunidad.estado}</span>
+                <p>Proximo seguimiento: <time class="time-seguimiento" datetime="${oportunidad.next}">${formatearFecha(oportunidad.next)}</time></p>
+                
+                
+            </div>
+            <div class="oportunidad-acciones">
+                <button class="editar" data-id="${oportunidad.id}">Editar</button>
+                <button class="eliminar" data-id="${oportunidad.id}">Eliminar</button>
+            </div>
+        </article>
+    `
+    }).join('');
+    listaOportunidades.innerHTML = cardsHTML;
+}
+
 //Limpiar formulario
 function limpiarFormulario() {
     formulario.reset();
@@ -73,4 +127,34 @@ function limpiarFormulario() {
 //Generar ID
 function generarId() {
     return Math.random().toString(36).substring(2) + Date.now()
+}
+
+//Formatear fecha
+function formatearFecha(fecha) {
+    const [anio, mes, dia] = fecha.split('-');
+    return `${dia}/${mes}/${anio}`;
+}
+
+//Tema oscuro/claro
+function aplicarTema(tema) {
+    document.documentElement.setAttribute('data-theme', tema);
+
+    if (tema === 'dark') {
+        themeIcon.src = 'assets/images/sun.png';
+        themeToggle.setAttribute('aria-label', 'Activar modo claro');
+    } else {
+        themeIcon.src = 'assets/images/moon.png';
+        themeToggle.setAttribute('aria-label', 'Activar modo oscuro');
+    }
+}
+function cambiarTema() {
+    const temaActual =
+        document.documentElement.getAttribute('data-theme');
+
+    const nuevoTema =
+        temaActual === 'dark' ? 'light' : 'dark';
+
+    aplicarTema(nuevoTema);
+
+    localStorage.setItem('theme', nuevoTema);
 }
